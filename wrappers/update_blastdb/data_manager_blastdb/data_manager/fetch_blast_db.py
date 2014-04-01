aea1ddeb5dcf51595b9ccafda4a79c996ba41faa
limic <limic@8f470391-0214-0410-be57-dbd57b47fce5>
2014-04-01 22:34:16 +0000

    Collapse tools into one, made major changes to script to suit the new tools.
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/Galaxy/trunk@4994 8f470391-0214-0410-be57-dbd57b47fce5

--- wrappers/update_blastdb/data_manager_blastdb/data_manager/fetch_blast_db.py
+++ wrappers/update_blastdb/data_manager_blastdb/data_manager/fetch_blast_db.py
@@ -6,37 +6,9 @@ import optparse
 import os
 import sys
 import subprocess
-import hashlib
+import time
 
 from galaxy.util.json import from_json_string, to_json_string
-DEFAULT_ALGORITHM = hashlib.sha512
-CHUNK_SIZE = 2**20 #1mb
-
-def get_dir_hash( directory, algorithm=None, followlinks=True, chunk_size=None ):
-    chunk_size = chunk_size or CHUNK_SIZE
-    algorithm = algorithm or DEFAULT_ALGORITHM
-    if isinstance( algorithm, basestring ):
-        hash = hashlib.new( algorithm )
-    else:
-        hash = algorithm()
-    #we hash a directory by taking names of directories, files and their contents
-    for dirpath, dirnames, filenames in os.walk( directory, followlinks=followlinks ):
-        dirnames.sort()
-        filenames.sort()
-        for name in dirnames:
-            hash.update( os.path.relpath( os.path.join( dirpath, name ), directory ) )
-        for name in filenames:
-            filename = os.path.join( dirpath, name )
-            hash.update( os.path.relpath( filename, directory ) )
-            fh = open( filename, 'rb' )
-            while True:
-                data = fh.read( chunk_size )
-                if not data:
-                    break
-                hash.update( data )
-            fh.close()
-    
-    return hash.hexdigest()
 
 def main():
     #Parse Command Line
@@ -45,30 +17,43 @@ def main():
     parser.add_option( '-t', '--tool_data_table_name', dest='tool_data_table_name', action='store', type='string', default=None, help='tool_data_table_name' )
     (options, args) = parser.parse_args()
     
+    #Take the JSON input file for parsing
     params = from_json_string( open( options.filename ).read() )
     target_directory = params[ 'output_data' ][0]['extra_files_path']
     os.mkdir( target_directory )
-    
-    blastdb_name = params['param_dict']['blastdb_name'] #value
+
+    #Fetch parameters from input JSON file    
+    blastdb_name = params['param_dict']['db_type'].get( 'blastdb_name' )
+    blastdb_type = params['param_dict']['db_type'].get( 'blastdb_type' )
     data_description = params['param_dict']['advanced'].get( 'data_description', None )
     data_id = params['param_dict']['advanced'].get( 'data_id', None )
     
+    #Run update_blastdb.pl
     cmd_options = [ '--decompress' ]
-    
     args = [ 'update_blastdb.pl' ] + cmd_options + [ blastdb_name ]
     proc = subprocess.Popen( args=args, shell=False, cwd=target_directory )
     return_code = proc.wait()
+
+    #Check if download was successful (exit code 1)
     if return_code != 1:
         print >> sys.stderr, "Error obtaining blastdb (%s)" % return_code
         sys.exit( 1 )
     
+    #Set id and description if not provided in the advanced settings
     if not data_id:
-        data_id = "%s_%s" % ( blastdb_name, get_dir_hash( target_directory ) )
+        #Use download time to create uniq id
+        localtime = time.localtime()
+        timeString = time.strftime("%Y%m%d%H%M%S", localtime)
+        data_id = "%s_%s" % ( blastdb_name, timeString )
     
     if not data_description:
         alias_date = None
         try:
-            for line in open( os.path.join( target_directory, "%s.nal" % ( blastdb_name ) ) ):
+            if blastdb_type is 'nucl':
+                alias_file = "%s.nal" % ( blastdb_name )
+            if blastdb_type is 'prot':
+                alias_file = "%s.pal" % ( blastdb_name )
+            for line in open( os.path.join( target_directory, alias_file ) ):
                 if line.startswith( '# Alias file created ' ):
                     alias_date = line.split( '# Alias file created ', 1 )[1].strip()
                 if line.startswith( 'TITLE' ):
@@ -76,12 +61,15 @@ def main():
                     break
         except Exception, e:
             print >> sys.stderr, "Error Parsing Alias file for TITLE and date: %s" % ( e )
+        #If we manage to parse the pal or nal file, set description
         if alias_date and data_description:
             data_description = "%s (%s)" % ( data_description, alias_date )
     
+    #If we could not parse the nal or pal file for some reason
     if not data_description:
         data_description = data_id
     
+    #Prepare output string to convert into JSON format
     data_table_entry = { 'value':data_id, 'name':data_description, 'path': os.path.join( blastdb_name, data_id ), 'database_alias_name': blastdb_name }
     data_manager_dict = { 'data_tables': { options.tool_data_table_name: [ data_table_entry ]  } }
     
