984fe25d3ffb70ca4d29f22722c883d883228db2
katbeaulieu <katherine.beaulieu014@gmail.com>
2016-07-04 13:03:08 -0500

    Add files via upload

--- wrappers/irods_pull/irods_pull_0_0_1.py
+++ wrappers/irods_pull/irods_pull_0_0_1.py
@@ -1,12 +1,12 @@
-
 from irods.exception import get_exception_by_code, NetworkException
 from irods.models import Collection,User,DataObject
 from irods.session import iRODSSession
 import password_obfuscation
 import json
 import exceptions
-
-
+import sys
+import os
+import string
 class irodsPull:
 	port = 0
 	host= ""
@@ -14,9 +14,11 @@ class irodsPull:
 	zone = ""
 	pw = ""
 	sess = None
-	outFile = None
+	outfileid = ""
+	newFilePath = ""
+	outfile = ""
 
-	def __init__(self,envFilePath,pwFilePath,outfile):
+	def __init__(self,envFilePath,pwFilePath,outfileid,newFilePath):
 		with open(pwFilePath) as f:
 			first_line = f.readline().strip()
 
@@ -29,31 +31,45 @@ class irodsPull:
 		self.username = str(data["irods_user_name"])
 		self.sess = iRODSSession(host=self.host, port=self.port, user=self.username, password=self.pw, zone=self.zone)
 		#this is just the filename
-		self.outFile = outfile
-
+		self.outfileid = str(outfileid)
+		self.newFilePath = str(newFilePath)
+		
 	def checkIfFileExists(self,filePath):
 		try:
-			obj=sess.data_objects.get(filePath)
+			obj=self.sess.data_objects.get(filePath)
 			return True
 		except:
-			sys.exit("You either do not have permission to access this file or the file does not exist")
+			sys.exit("One of the files you selected either does not exist or you do not have read access.")
 			
-	def pull_and_push(self,filePath):
-		answer = self.sess.data_objects.get(filePath)
-		with answer.open('r+') as f:
-			data = f.read()
+	def pull_and_push(self,filePaths):
 
-		with open(self.outFile,"w") as fout:
-			fout.write(data)
-			#f.seek(0,0)
-		return self.outFile
+		for filePath in filePaths:	
+			if filePath[0] != '/':
+				filePath = '/' + filePath
+			if filePath[-1] == '/':
+				filePath = filePath[0:len(filePath)-1]
+			pathList = filePath.split('/')
+			firstPath, file_ext = os.path.splitext(filePath)
+			filePathForGalaxy = string.replace(pathList[-1],"_","-")			
+			filename = "primary_" + self.outfileid +"_" + filePathForGalaxy + "_visible_" + str(file_ext[1:])
+			#may need to insert escape characters for spaces.
+			#exists = self.checkIfFileExists(filePath)
+			#print exists
+			answer = self.sess.data_objects.get(filePath)
+			#print str(answer)
+			#print filePath
+			#print str(answer)
+			try:
+				with answer.open('r') as f:
+					data = f.read()
+			except:	
+				print "The file: " + filePath + " either does not exist or you do not have read access permissions."
+				sys.exit(" ")
 
-
-
-				
-
-
-
-
+			with open(filename,"a+") as fout:
+				fout.write(data)
+				#f.seek(0,0)
+		self.sess.cleanup()
+#		return self.outFile
 
 
