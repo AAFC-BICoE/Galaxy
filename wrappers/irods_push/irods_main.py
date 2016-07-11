0743c38f814ba6b26bec1f01855a15b32ca5d472
katbeaulieu <katherine.beaulieu014@gmail.com>
2016-07-11 06:11:40 -0500

    Add files via upload

--- wrappers/irods_push/irods_main.py
+++ wrappers/irods_push/irods_main.py
@@ -1,6 +1,8 @@
 import sys
 import irods_push_0_0_2
 import getpass
+#from galaxy import datatypes
+import os.path,time
 #for now the assumption is that the user is using this tool in a linux environment
 #sys.argv[1] is the directory path where the files will be placed, and sys.argv[-1] is the 'no clobber' flag.
 #everything in between is file paths and file names
@@ -8,18 +10,36 @@ pwFile = "/home/" + getpass.getuser() + "/.irods/.irodsA"
 envFile = "/home/" + getpass.getuser() + "/.irods/irods_environment.json"
 
 
-newIrodsObject = irods_push_0_0_2.irodsCredentials(envFile,pwFile,sys.argv[-1])
+#the last item will be the 'metadata' object
+
+
+newIrodsObject = irods_push_0_0_2.irodsCredentials(envFile,pwFile,sys.argv[-5])
 #newIrodsObject.checkIfCollectionExists(sys.argv[1])
 newIrodsObject.addDirectories(sys.argv[1])
 
 filePaths = []
 fileNames = []
-for x in range(2,len(sys.argv)-3,2):
+for x in range(2,len(sys.argv)-7,2):
+#	print sys.argv[x+1]	
 	filePaths.append(sys.argv[x])
 	fileNames.append(sys.argv[x+1])
-newIrodsObject.addFiles(filePaths,fileNames,sys.argv[1],sys.argv[-2])
-print filePaths
-print fileNames
-print sys.argv[1]
-print sys.argv[-2]
+newIrodsObject.addFiles(filePaths,fileNames,sys.argv[1],sys.argv[-6])
+#print filePaths
+#print fileNames
+#print sys.argv[1]
+#print sys.argv[-4]
+print str(sys.argv[-1])
 
+listOfMeta = []
+#sys.argv[-1] is the tool_id
+listOfMeta.append(sys.argv[-4])
+listOfMeta.append(sys.argv[-3])
+listOfMeta.append(sys.argv[-2])
+listOfMeta.append(str(sys.argv[-1]))
+#print "Trying to print uuid: " + str(sys.argv[-1])
+print str(filePaths)
+creation_date = time.ctime(os.path.getctime(filePaths[0]))
+listOfMeta.append(creation_date)
+listOfMeta.append(str(os.path.getsize(filePaths[0])))
+#listOfMeta.append(sys.argv[-1])
+newIrodsObject.addMetadata(fileNames,sys.argv[1],listOfMeta)
