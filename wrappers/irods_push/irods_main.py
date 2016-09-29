882db6c8ec915ccbdc05aa2e56b336770c78a546
katbeaulieu <katherine.beaulieu014@gmail.com>
2016-09-29 13:51:12 -0400

    Clean up of files

--- wrappers/irods_push/irods_main.py
+++ wrappers/irods_push/irods_main.py
@@ -1,55 +1,27 @@
 import sys
 import irods_push_0_0_2
-import getpass
-#from galaxy import datatypes
 import os.path,time
-print " I got here"
-#for now the assumption is that the user is using this tool in a linux environment
-#sys.argv[1] is the directory path where the files will be placed, and sys.argv[-1] is the 'no clobber' flag.
-#everything in between is file paths and file names
-#pwFile = "/home/" + getpass.getuser() + "/.irods/.irodsA"
-#envFile = "/home/" + getpass.getuser() + "/.irods/irods_environment.json"
 
-#
-#index = 1
-#for arg in sys.argv:
-#	print "index: " + str(index) + " Arg: " + arg
-#	index = index+1
-	
 
+#Set variables from command line
 directoryPath = sys.argv[1]
-print "argv1: " + directoryPath
 filePath = sys.argv[2]
-print "argv2: " + filePath
 fileName = sys.argv[3]
-print "argv3: " + fileName
 noclobber = sys.argv[4]
-print "argv4: " + noclobber
 outputName = sys.argv[5]
-print "argv5: " + outputName
 historyContentId = sys.argv[6]
-print "arg6: " + historyContentId
 historyId = sys.argv[7]
-print "arg7: " + historyId
 outputExt = sys.argv[8]
-print "argv8: " + outputExt
 toolId = sys.argv[9] 
-print "argv9: " + toolId
 hostname = sys.argv[10]
-print "argv10: " + hostname
 port = sys.argv[11]
-print "argv11: " + port
 zone = sys.argv[12]
-print "argv12: " + zone
 username = sys.argv[13] 
-print "arg13: " + username
 password = sys.argv[14]
-print "arg14: " + password
 metadataList = sys.argv[15]
-print "arg15: " + metadataList
 resource = str(sys.argv[-1])
-print "arg-1: " + resource
-print "len: " + str(len(sys.argv))
+#set two lists containing the keys and values
+#the minimal length of the command line arguement list is 17
 if len(sys.argv) > 17 and sys.argv[15] != "blank":
 	lenArgs = len(sys.argv)
 	x = 15
@@ -59,28 +31,23 @@ if len(sys.argv) > 17 and sys.argv[15] != "blank":
 		keys.append(sys.argv[x])
 		values.append(sys.argv[x+1])
 		x = x+2
-	print "keys: " + str(keys)
-	print "values: " + str(values)
-#length by default is 17
 
-#the last item will be the 'metadata' object
-#11 with resource name I think...
-
+#create object which will have irods session object.
 newIrodsObject = irods_push_0_0_2.irodsCredentials(hostname,port,zone,username,password,outputName)
+#create new collection, if it doesn't already exist
 newIrodsObject.addDirectories(directoryPath)
+#add filepath to a list with only one item, only did this because I didn't want to rewrite 
+#the irods_push functions
 filePath = [filePath]
+#same deal as for filename, the filename was put into a list because of previous implementation
+#of irods_push functions
 fileName = [fileName]
-#filePaths = []
-#fileNames = []
-#for x in range(2,len(sys.argv)-13,2):
-	
-#	filePaths.append(sys.argv[x])
-#	fileNames.append(sys.argv[x+1])
 
 newIrodsObject.addFiles(filePath,fileName,directoryPath,noclobber,resource)
 
+#add auto-generated metadata to a list to be added one by one, in this case we 
+#have to define the key for these values.
 listOfMeta = []
-
 listOfMeta.append(toolId)
 listOfMeta.append(historyContentId)
 listOfMeta.append(historyId)
@@ -89,6 +56,7 @@ listOfMeta.append(outputExt)
 creation_date = time.ctime(os.path.getctime(filePath[0]))
 listOfMeta.append(creation_date)
 listOfMeta.append(str(os.path.getsize(filePath[0])))
-
+#add automatic defined metadata
 newIrodsObject.addMetadata(fileName,directoryPath,listOfMeta)
+#add user defined metadata
 newIrodsObject.addMetadataFromList(fileName,directoryPath,keys,values)
--- wrappers/irods_push/irods_push_0_0_2.py
+++ wrappers/irods_push/irods_push_0_0_2.py
@@ -1,7 +1,5 @@
 '''
 This class uses the python-irodsclient api. Make sure it is installed on your computer. This class also uses the 
-password_obfuscation.py file which can be found on the irods github main page. It requires you to have the 
-module 'six' installed.
 '''
 import sys
 import os
@@ -9,9 +7,6 @@ import exceptions
 from irods.exception import get_exception_by_code, NetworkException
 from irods.models import Collection,User,DataObject
 from irods.session import iRODSSession
-#import password_obfuscation 
-#import getpass
-import json
 
 class FileDuplicateError(Exception):
 	pass
@@ -19,9 +14,9 @@ class FileDuplicateError(Exception):
 class HierarchyError(Exception):
 	pass
 
-
-#TODO add a file as an attribute of the class and have the file get filled as functions are called, then at the end get that attribute and make it as the galaxy output
-#TODO figure out a way to make class attributes in python private.
+'''
+Class to hold irods credentials and create irods session.
+'''
 class irodsCredentials:
 	port = 0
 	host = ""
@@ -30,18 +25,8 @@ class irodsCredentials:
 	pw = ""
 	sess = None
 	outFile = None	
-	#initialize your irods session by reading off irods credentials from .irodsA file and from irods_environment.json file
+	#Make sure there are no errors with the credentials at this point.
 	def __init__(self,hostname,port,zone,username,password,outFile):
-#		with open(pwFilePath) as f:
-#			first_line = f.readline().strip()
-		#decode scrambled password from .irodsA file using decode function
-#		self.pw = password_obfuscation.decode(first_line)
-#		with open(envFilePath) as f:
-#			data = json.load(f)
-#		self.port = str(data["irods_port"])
-#		self.host = str(data["irods_host"])
-#		self.zone = str(data["irods_zone_name"])
-#		self.username = str(data["irods_user_name"])
 		self.host = str(hostname)
 		try:
 			float(port)
@@ -57,24 +42,14 @@ class irodsCredentials:
 			col = self.sess.collections.get("/" + self.zone + "/home/" + self.username)
 		except NetworkException:
 			sys.exit("Invalid user credentials")
-		#print "Just created: " + str(self.sess.pool.active)
-		#print "Just created: " + str(self.sess.pool.idle)
 		self.outFile = outFile
-	#Established so no user should be able to put something in anything higher than their main root directory. 
-	def basicPath(self):
-		return "/" + self.zone + "/home/" + self.username + "/"
 
-	#TODO add a check for the main root directories, making sure an error is thrown if the program is passed any of the following directories: zone, home, or username directory.
 	#check if collection exists, if it does, return True, otherwise False
 	def checkIfCollectionExists(self,dirName):
-		#print "Before I do any stripping of slashes: " + dirName
 		if dirName[0] != '/':
 			dirName = '/' + dirName
 		if dirName[-1] == '/':
 			dirName = dirName[0:len(dirName)-1]
-		#print "I am checking that this exists: " + dirName
-#		print "dirName in checkColl: " + dirName	
-		#if the dirname they are checking is /zone, /zone/home, or /zone/home/user, they may not have permission to query these so just skip them
 		if dirName == "/" + self.zone or dirName == "/" + self.zone + "/home" or dirName == "/" +self.zone + "/home/" + self.username:
 			return True
 		query = self.sess.query(Collection).filter(Collection.name == dirName)
@@ -84,7 +59,6 @@ class irodsCredentials:
         	                f.write("Collection '" + dirName + "' doesn't exist\n")
 			return False
 		with open(self.outFile,"a+") as f:
-			#print dirName
                         f.write("Collection '" + dirName + "' exists\n")
 		return True
 	#Split up the string of directories delimited by '/' and create collections within collections until there are no more directories.	
@@ -97,13 +71,9 @@ class irodsCredentials:
 			strList = dirName.split('/')
 			if strList[0] == "":
 				del strList[0]
-			#print str(strList)
 			f = open(self.outFile,"a+")
 			currentPath = "/"
 			for t in range(len(strList)):
-				#only bother creating a new collection within the collection if it doesn't already exist	
-				#print "Print current path + strList: " + currentPath + strList[t]
-				#print "Strlist[t]: " + strList[t]
 				if self.checkIfCollectionExists(currentPath + strList[t]) == False:
 					f.write("Created collection: " + currentPath + strList[t] + "\n")
 					try:	
@@ -135,8 +105,6 @@ class irodsCredentials:
 			#file doesn't exist so create it, don't need to check for 'no clobber'
 			if len(results) <= 0:
 				myOutFile.write("File '" + fileNames[x] + "' does not exist. Creating it in irods.\n")
-				#print dirName
-				#print self.basicPath() + dirName + "/" + fileNames[x]
 				if resourceName != "blank":
 					try:
 						obj = self.sess.data_objects.create(dirName + "/" + fileNames[x], resourceName)
@@ -146,26 +114,11 @@ class irodsCredentials:
 					try:
 						obj = self.sess.data_objects.create(dirName + "/" + fileNames[x])
 					except: 		
-						print "file path: " + dirName + "/" + fileNames[x]
-						print "Resource name: " + resourceName
 						sys.exit("You do not have permission to write to this directory path")
 		
-				#if resource == " ":
-				#	try:
-				#		obj = self.sess.data_objects.create(dirName + "/" + fileNames[x])
-				#	except:
-				#		sys.exit("You do not have permission to write to this directory path")
-				#else:
-				#	try:
-				#		obj = self.sess.data_objects.create(dirName + "/" + fileNames[x], resource=resource)
-				#	except:
-				#		sys.exit("You do not have permission to write to this directory path")
-				
 				with open(filePaths[x]) as fileToPlace:
 					data = fileToPlace.read()
-				#print self.basicPath() + dirName + "/" + fileNames[x]
 				obj = self.sess.data_objects.get(dirName + "/" + fileNames[x])
-				#print "Pathname before error: " + dirName + "/" + fileNames[x]
 				with obj.open('w+') as f:
 					f.write(data)			
 					f.seek(0,0)
@@ -178,7 +131,6 @@ class irodsCredentials:
 					myOutFile.write("File '" + fileNames[x] + "' already exists. Overwriting now.\n")
 					obj = self.sess.data_objects.get(dirName + "/" + fileNames[x])
 					data = ""
-					#print "Pathname before error: " +dirName +"/" + fileNames[x]
 					with obj.open('w+') as f:
 						f.write(data)
 			
@@ -192,10 +144,8 @@ class irodsCredentials:
 					myOutFile.write("Successfully overwrote: " + fileNames[x] + "\n")
 				else:
 					myOutFile.write("File '" + fileNames[x] + "' already exists. Prevented from overwriting\n")
-	#	self.sess.cleanup()			
 
 	def addMetadata(self,fileName,dirName,metadata):
-#		print fileName[0]
 		obj = self.sess.data_objects.get(dirName + "/" + fileName[0])	
 		listOfMeta = obj.metadata.get_all('tool_id') 
 		if listOfMeta > 0:
@@ -227,7 +177,7 @@ class irodsCredentials:
 			for i in listOfMeta:
 				obj.metadata.remove(i)
 		obj.metadata.add('size',metadata[5])	
-
+	#add metadata to file from lists of user generated metadata.
 	def addMetadataFromList(self,fileName,dirName,keys,values):
 		if len(keys) < 0 and len(values) < 0:
 			self.sess.cleanup()
@@ -243,17 +193,6 @@ class irodsCredentials:
 		self.sess.cleanup()
 	
 
-#newIrods = irodsCredentials("/home/katherine/.irods/irods_environment.json", "/home/katherine/.irods/.irodsA","outfile.txt")
-#:q
-#newIrods.addDirectories("/tempZone/home/rods/katDir/newDir/files")
-
-#filePaths = []
-#fileNames = []
-#filePaths.append("/home/katherine/galaxyTool/randomFiles/file1.txt")
-#filePaths.append("/home/katherine/galaxyTool/randomFiles/file2.txt")
-#fileNames.append("file1.txt")
-#fileNames.append("file2.txt")
-#newIrods.addFiles(filePaths,fileNames,"/tempZone/home/rods/katDir/newDir/files","true")
 
 
 
