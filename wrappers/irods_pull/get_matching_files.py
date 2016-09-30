1bcd90422bee3d37749ead313006e88c81c99215
katbeaulieu <katherine.beaulieu014@gmail.com>
2016-09-30 08:10:22 -0400

    Clean up of files

--- wrappers/irods_pull/get_matching_files.py
+++ wrappers/irods_pull/get_matching_files.py
@@ -3,14 +3,13 @@ from irods.exception import get_exception_by_code, NetworkException
 from irods.models import Collection,User,DataObject, DataAccess
 from irods.session import iRODSSession
 		
-
+'''
+Find files that match search criteria. First eliminate files based on substring,
+then based on key value pairs. If there is not substring, just match on key value
+pairs and if there are no key value pairs, match only one substring.
+'''
 def get_matching_files(hostname,port,zone,username,password,substring,keyvalsList):
 	matchFiles = []
-#	hostname = "katherine-VirtualBox"
-#	port="1247"
-#	zone="tempZone"
-#	username="rods"
-#	password="testpassword"
 	sess = iRODSSession(host=str(hostname),port=str(port),user=str(username),password=str(password),zone=str(zone))
 	accessible_files = []
 	query = sess.query(Collection.name, DataObject.name).filter(DataAccess.type >= '1050').all()
@@ -20,28 +19,17 @@ def get_matching_files(hostname,port,zone,username,password,substring,keyvalsLis
 		
 
 	matchingFiles = []
-#	print "substring: " + substring
-	#print "sub len: " + str(len(substring))
 	if len(substring) >= 1:
 		for item in accessible_files:
 			if item.find(str(substring)) != (-1):
 				matchFiles.append(item)
-#				matchingFiles.append((item,item,0))
-	#	print "in substring check: " + str(matchFiles)	
-	#keyvalsList is a list of dictionaries, with indices 'key' and 'value'
 	if len(keyvalsList) >= 1:
-#		print keyvalsList
 		if str(keyvalsList[0]["key"]) == '' and str(keyvalsList[0]["value"]) == '':
 			keyvalsList = []
-	#print "keyval list now: " + str(keyvalsList)
-	#print "len key: " + str(len(keyvalsList))
-	#print "len match: " + str(len(matchFiles))
 	if len(matchFiles) < 1 and len(keyvalsList) < 1:
-	#	print "both empty"
 		sess.cleanup()
 		return []
 	elif len(keyvalsList) < 1 and len(matchFiles) >= 1:
-	#	print "match filled, key empty"
 		sess.cleanup()
 		filesToMatch = []
 		for item in matchFiles:
@@ -51,104 +39,30 @@ def get_matching_files(hostname,port,zone,username,password,substring,keyvalsLis
 		matchFiles = accessible_files
 	endFiles = []
 	if len(keyvalsList) >= 1:
-		print "matchfiles: " + str(matchFiles)
 		for filename in matchFiles:
 			
-			print str(filename) + ":"
 			obj = sess.data_objects.get(filename)	
 			flag = True
 			foundAFalse = False
 			for item in keyvalsList:
-#				print str(item) + ":"
-				#print obj.metadata.get_all(str(item["key"]))
 				listOfMatching = obj.metadata.get_all(str(item["key"]))
-#				print str(listOfMatching)
-#				print "type: " + str(type(listOfMatching[0]))
 				if len(listOfMatching) < 1:
 					flag = False
-#					print str(flag)	
 				else:
 					for matchingMeta in listOfMatching:
-						
 						value = matchingMeta.value
-						
-						print "value: " + str(value)	
-						print "item['value']: " + str(item["value"])
 						if len(str(item["value"])) < 1:
 							flag = True
 						elif str(value) == str(item["value"]):
 							flag = True
-#							print str(flag)
 						else:
 							flag = False
 							foundAFalse = True
-				
 			if foundAFalse == False:
 				endFiles.append(filename)
 	matchingFiles = []
 	for item in endFiles:
 		matchingFiles.append((item,item,0))
 
-	#filesToReturn = []
-	#for item in accessible_files:
-	#	filesToReturn.append((item,item,0))
 	sess.cleanup()
 	return matchingFiles
-	'''
-	flag = True
-	finalMatching = []
-	if matchFiles is not []:
-		print "matchFiles: " + str(matchFiles)
-	
-	for item in matchingFiles:
-		obj = sess.data_objects.get(item)
-		if keyvalsList is not []:
-			if keyvalsList[0]["key"] is not '' and keysvalsList[0]["value"] is not '' :
-				print "keyvalsList: " + str(keyvalsList)	
-				for item in keyvalsList:
-					flag = True
-					if (type(item) is dict) and (bool(item)):
-						print item["key"]
-						print item["value"]
-						#this means it has meta with that key, not necessarily the right value
-						exists = obj.metadata.get_all(str(item["key"]))
-						if exists is  []:
-							flag = False
-				if flag == True:
-					finalMatching.append((item,item,0))
-		else:
-			sess.cleanup()
-			return matchFiles
-
-
-#						if flag:
-#							if str(item["value"]) == "":
-#								flag = True 
-							#else if the value doesn't match, flag = False
-				#if flag is false at this point, don't add the file to the list	
-	'''				
-#	outputList = []
-#	outputList.append((str(substring),str(substring),0))
-#	for i in keyvalsList:
-#		
-#		outputList.append((str(i["key"]),str(i["key"]),0))
-#		
-#		outputList.append((str(i["value"]),str(i["value"]),0))
-#	outputList.append((str(hostname),str(hostname),0))
-#	outputList.append((str(port),str(port),0))
-#	outputList.append((str(username),str(username),0))
-#	outputList.append((str(password),str(password),0))
-#	return outputList
-#result = get_matching_files("katherine-VirtualBox","1247","tempZone","rods","testpassword","rods","some")
-#print str(result)
-'''
-def get_matching_files(substring):
-        outputList = []
-        outputList.append((substring,substring,0))
-        #for i in keyvalsList:
-        #        if i["key"]:    
-         #               outputList.append((i["key"],i["key"],0))
-          #      elif i["value"]:
-           #             outputList.append((i["value"],i["value"],0))
-        return outputList
-'''
--- wrappers/irods_pull/new_import.py
+++ wrappers/irods_pull/new_import.py
@@ -7,23 +7,18 @@ if not os.path.exists(sys.argv[-1]):
 if sys.argv[6] == 'None':
 	sys.exit("Please specify file(s) to import")
 totalList = []
+'''
+Take all the file names starting from argument 6 and add to a list to iterate through and
+add to the irods filesystem.
+'''
 x = 6
 while x < len(sys.argv)-1:
 	print "x:" + str(x)
 	print "sys.argv[x]: " + sys.argv[x]
 	myList = sys.argv[x].split(",")
-#	totalList.extend(myList)
 	totalList = totalList + myList
 	x = x + 1
-#myList = sys.argv[-2].split(",")
-
-#this takes the string of filenames and makes it into a list of filenames
-#for x in range(len(myList)):
-#	myList[x] = myList[x].rsplit('/',1)[-1]
 
 
-#for name in myList:
-#	with open(os.path.join(sys.argv[-1],name),'w') as out:
-#		out.write("Hello world")
 newIrods = irods_pull_0_0_1.irodsPull(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
 newIrods.pull_and_push(totalList,sys.argv[-1])
