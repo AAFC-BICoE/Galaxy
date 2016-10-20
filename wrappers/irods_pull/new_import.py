11eb4ab372ded23b3084f036c9e029d3d1e042f4
katbeaulieu <katherine.beaulieu014@gmail.com>
2016-10-20 13:02:55 -0400

    Added change for password env variable.
    changed command line arguments

--- wrappers/irods_pull/new_import.py
+++ wrappers/irods_pull/new_import.py
@@ -4,14 +4,22 @@ import irods_pull_0_0_1
 if not os.path.exists(sys.argv[-1]):
 	os.makedirs(sys.argv[-1])
 
-if sys.argv[6] == 'None':
+print "sys.argv1: " + sys.argv[1]
+print "sys.argv2: " + sys.argv[2]
+print "sys.argv3: " + sys.argv[3]
+print "sys.argv4: " + sys.argv[4]
+print "sys.argv5: " + sys.argv[5]
+print "sys.argv6: " + sys.argv[6]
+
+
+if sys.argv[5] == 'None':
 	sys.exit("Please specify file(s) to import")
 totalList = []
 '''
 Take all the file names starting from argument 6 and add to a list to iterate through and
 add to the irods filesystem.
 '''
-x = 6
+x = 5
 while x < len(sys.argv)-1:
 	print "x:" + str(x)
 	print "sys.argv[x]: " + sys.argv[x]
@@ -19,6 +27,7 @@ while x < len(sys.argv)-1:
 	totalList = totalList + myList
 	x = x + 1
 
-
-newIrods = irods_pull_0_0_1.irodsPull(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
+password =  os.environ['PASS']
+print password
+newIrods = irods_pull_0_0_1.irodsPull(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],password)
 newIrods.pull_and_push(totalList,sys.argv[-1])
